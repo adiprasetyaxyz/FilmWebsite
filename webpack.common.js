376a83b563541fa9b502a1ba9563aec03d8e4f1f
unknown <adiprasetyaxyz@icloud.com>
2024-08-29 23:06:14 +0800

    fix Error

--- webpack.common.js
+++ webpack.common.js
@@ -1,41 +1,35 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
- 
 module.exports = {
-  entry: './index.js',
+  entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
- 
+
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader",
-        ]
-      }
-    ]
-  },  
+      },
+    ],
+  },
   plugins: [
     /* HTML Webpack Plugin */
     new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
+      template: "./index.html",
+      filename: "index.html",
     }),
     new CopyPlugin({
       patterns: [
         {
-          from: __dirname + '/assets/img',
-          to: 'assets/img',
-          noErrorOnMissing: true
-      }
-         ],
+          from: __dirname + "/assets/img",
+          to: "assets/img",
+          noErrorOnMissing: true,
+        },
+      ],
     }),
   ],
-  
 };
